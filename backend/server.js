5723e8f44269772fab9bd3f5e2426c940f260acf
owofuyuki <hieu.npt1710@gmail.com>
2023-08-11 08:06:15 +0700

    Updating..

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import cookieParser from "cookie-parser";
 
+const salt = 10;
+
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -17,6 +19,23 @@ const confidentalDatabase = mysql.createConnection({
     database: "signup",
 });
 
-app.listen(8080, () => {
+app.post("/signup", (req, res) => {
+    const sql = "INSERT INTO login (`info`,`phone`,`username`,`password`) ACCOUNT (?)";
+    bcrypt.hash(req.body.password.toString(), salt, (err, hash) => {
+        if (err) return res.json({Error: "Error for hassing password."});
+        const account = [
+            req.body.info,
+            req.body.phone,
+            req.body.username,
+            hash,
+        ];
+        confidentalDatabase.query(sql, [account], (err, result) => {
+            if (err) return res.json({Error: "Inserting data Error in server."});
+            return res.json({Status: "Success."});
+        });
+    });
+});
+
+app.listen(8033, () => {
     console.log("Running...");
 });
